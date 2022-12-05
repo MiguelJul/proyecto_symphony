857fbea1ae04fdeffc3b17167033d442f80e137c
MiguelJul <migueljulia123@gmail.com>
2022-12-05 19:47:15 +0100

    Commit Twig EquipsMiguel

--- src/Controller/EquipsController.php
+++ src/Controller/EquipsController.php
@@ -2,21 +2,22 @@
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-class ContacteController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+class EquipsController extends AbstractController
 {
 private $equips = array(
     array("codi" => "1", "nom" => "Equip Roig", "cicle" =>"DAW",
     "curs" =>"22/23", "membres" =>
-    array("Elena","Vicent","Joan","Maria")),
+    array("Elena","Vicent","Joan","Maria"),"foto"=>"Green/assets/img/rojo.jpg"),
     array("codi" => "2", "nom" => "Equip Blau", "cicle" =>"DAM",
     "curs" =>"22/23", "membres" =>
-    array("Carlos","Irene","Lorena","Ricardo")),
+    array("Carlos","Irene","Lorena","Ricardo"),"foto"=>"Green/assets/img/azul.jpg"),
     array("codi" => "3", "nom" => "Equip Groc", "cicle" =>"ASIR",
     "curs" =>"22/23", "membres" =>
-    array("Toni","Ximo","Paula","Laura")),
+    array("Toni","Ximo","Paula","Laura"),"foto"=>"Green/assets/img/verde.jpeg"),
     array("codi" => "4", "nom" => "Equip Negre", "cicle" =>"ASIX",
     "curs" =>"22/23", "membres" =>
-    array("Marc","Sara","Roman","Aitor"))
+    array("Marc","Sara","Roman","Aitor"),"foto"=>"Green/assets/img/negro.jpg")
 );
 
 #[Route('/equip/{codi}' ,name:'dades_equip', requirements: ['codi' => '\d+'])]
@@ -27,23 +28,13 @@ function($equip) use ($codi)
 {
 return $equip["codi"] == $codi;
 });
+
 if (count($resultat) > 0)
-{
- 
-$resposta = "";
-$resultat = array_shift($resultat); #torna el primer element
-$membres="";
-foreach($resultat["membres"] as $membre)
-$membres.=$membre.", ";
-$resposta .= "<ul><li>" . $resultat["nom"] . "</li>" .
-"<li>" . $resultat["cicle"] . "</li>" .
-"<li>" . $resultat["curs"] . "</li>".
-"<li>" . $membres . "</li>".
-"</ul>";
-return new Response("<html><body>$resposta</body></html>");
-}
+return $this->render('equip.html.twig', array(
+    'contactes' => array_shift($resultat),'codi'=>$codi));
 else
-return new Response("No s’ha trobat l’equip: ".$codi );
+return $this->render('equip.html.twig', array(
+    'contactes' => NULL,'codi'=>NULL));
 }
 
 #[Route('/equip/{text}' ,name:'buscar_equips')]
